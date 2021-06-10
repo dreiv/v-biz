292a489d6f989a37943ae35926c01f48a350f91b
Andrei Voicu <dreivoicu@gmail.com>
2021-06-10 16:51:04 +0300

    added services

---
+++ src/services/articles/articles.types.ts
@@ -0,0 +1,13 @@
+import { IArticle, ICommentData, IArticleData } from "@/entities";
+
+export interface IArticlesService {
+  getAll(): IArticleData[];
+  getOneById(id: number): IArticle | undefined;
+  createComment(id: number, data: ICommentData): IArticle;
+}
+
+export interface IArticlesServiceMock {
+  getAll: jest.Mock<IArticleData[]>;
+  getOneById: jest.Mock<IArticle | undefined>;
+  createComment: jest.Mock<IArticle>;
+}
